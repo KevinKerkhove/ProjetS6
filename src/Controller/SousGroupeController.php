fdfcb6cba08816b97263e6f4b2d966d175f2e1b6
Kεяκнσνε Kενıп(C'εƨт Bεпɢ Bεпɢ) <kevin_kerkhove@ens.univ-artois.fr>
2020-12-08 11:00:12 +0100

    changement de route pour les sous groupe

--- src/Controller/SousGroupeController.php
+++ src/Controller/SousGroupeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/sous/groupe")
+ * @Route("/sous_groupe")
  */
 class SousGroupeController extends AbstractController
 {
